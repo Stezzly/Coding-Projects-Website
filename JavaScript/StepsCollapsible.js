f779c61ff2bb7e912ba0914705b0e3be13bda9bb
Stezzly <43850376+Stezzly@users.noreply.github.com>
2024-05-15 22:30:24 -0230

    Coding - C Introduction Complete
    Coding section - C Introduction Complete

--- JavaScript/StepsCollapsible.js
+++ JavaScript/StepsCollapsible.js
@@ -43,15 +43,5 @@ document.addEventListener("DOMContentLoaded", function() {
 
     updateProgressAndConclusion(); // Initial check on page load to set the right state
 
-    // Function to toggle dark mode
-    var toggleDarkMode = function() {
-        document.body.classList.toggle('dark-mode');
-        document.body.classList.toggle('light-mode');
-    };
-
-    // Add event listener for dark mode toggle
-    var darkModeToggleBtn = document.getElementById('darkModeToggleBtn');
-    if (darkModeToggleBtn) {
-        darkModeToggleBtn.addEventListener('click', toggleDarkMode);
-    }
+  
 });
